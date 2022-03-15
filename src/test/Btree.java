54a0ba192166b7692ce4e3a0c89d5ace4ec812b3
leelamani8019 <leelamani081@gmail.com>
2022-03-15 10:45:50 +0530

    this is in demotest

---
+++ src/test/Btree.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Btree {
+	int empno;
+	String ename,dept;
+	Btree left;
+	Btree right;
+	public Btree(int empno,String ename,String dept) {
+		this.empno=empno;
+		this.ename=ename;
+		this.dept=dept;
+		
+		
+	}
+
+}
